243575c4be13e45e8d4f4744f9c442a39d80d4f6
sjb720 <sjbuonamassa@gmail.com>
2020-06-16 17:03:22 -0400

    maybe fixed chat

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d2ee12b4f18c93e8cace";
+/******/ 	var hotCurrentHash = "67870efc717ff220a3a8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
